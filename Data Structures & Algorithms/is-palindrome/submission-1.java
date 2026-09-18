1098063515af73b4945dcb3ec2575073fa30038c
Lino Larios <lino.larios@gmail.com>
2026-09-18 16:04:03 -0600

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.isEmpty()) return false;
+        if(s.isBlank()) return true;
+
+        int left =0;
+        int right = s.length()-1;
+
+        while(left<right){
+            while(left< right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left< right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(s.toLowerCase().charAt(left)!=s.toLowerCase().charAt(right)) return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
